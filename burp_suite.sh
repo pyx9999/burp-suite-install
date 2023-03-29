6aa2e4f4e1ace31bb2762b5d1d13d6589ae6f750
Eric Valdez Jr <128732768+pyx9999@users.noreply.github.com>
2023-03-29 14:09:09 -0700

    Update burp_suite.sh

--- burp_suite.sh
+++ burp_suite.sh
@@ -2,8 +2,34 @@
 
 #run after downloading burpsuite
 
-cd ~
-find Downloads -name "*.sh" -exec chmod +x {} \;
-cd Downloads
-./burpsuite_community_linux_*.sh
+echo $(whoami) "...have you downloaded burpsuite??? "
 
+function confirm() {
+    while true; do
+        read -p "Do you want to proceed? (YES/NO/CANCEL) " yn
+        case $yn in
+            [Yy]* ) return 0;;
+            [Nn]* ) return 1;;
+            [Cc]* ) exit;;
+            * ) echo "Please answer YES, NO, or CANCEL.";;
+        esac
+    done
+}
+ 
+# Example usage of the confirm function
+if confirm; then
+    echo "User chose YES. Executing the operation..."
+    cd
+    cd Downloads
+    chmod +x ./burpsuite_community_linux_*.sh
+    ./burpsuite_community_linux_*.sh
+
+else
+    echo "User chose NO. Aborting the operation..."
+    exit
+fi
+
+
+cd Downloads
+chmod +x ./burpsuite_community_linux_*.sh
+./burpsuite_community_linux_*.sh
